3b7a82990cbfa91f3e5d312d9f222f1b92fec8df
David L <dalaw85@gmail.com>
2019-03-05 14:06:56 -0500

    changes to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     var topics = ["beer", "sailing", "ocean", "food"];
     var image;
+    var imageURL;
+    var downloadButton;
+    var downloadFileName;
+    var favorite;
 
 function gifButton(){   
     $("#gif-buttons").empty();
@@ -15,10 +19,18 @@ function gifButton(){
     }
 }
 
+function downloadGif(){
+        downloadFileName.download({
+        url : imageURL,
+        filename : downloadFileName,
+        conflictAction : "prompt"
+    });
+}
+
 $("#add-gif").on("click", function() {
     event.preventDefault();
     var newButton =  $("#gif-input").val().trim();
-    gifs.push(newButton);
+    topics.push(newButton);
     gifButton();    
 })
 
@@ -38,38 +50,59 @@ function gifSearch() {
             for ( var i = 0; i < results.length; i++) { 
 
                 var gifDiv = $("<div>");
+                gifDiv.addClass("each-gif");
+                var innerDiv =$("<div>");
                 var rating = results[i].rating;
                 var p = $("<p>").text("Rating: " + rating);
+                p.addClass("rating");
+                downloadFileName = results[i].images.fixed_height.mp4;
+                downloadButton = $("<button>").text("Download");
+                downloadButton.attr("file", downloadFileName);
+                downloadButton.addClass("download-button my-buttons");
+                favorite = $("<button>").text("favorite");
+                favorite.addClass("favorite-button my-buttons");
                 image = $("<img>");
-                var imageURL = results[i].images.fixed_height.url
+                imageURL = results[i].images.fixed_height.url
+                var imageURLStill = results[i].images.fixed_height_still.url
                 image.attr("src", imageURL );
-                gifDiv.prepend(p);
+                image.attr("data-still", imageURLStill);
+                image.attr("data-animate", imageURL);
+                image.addClass("myGif");
+                innerDiv.prepend(favorite)
+                innerDiv.prepend(downloadButton);
+                innerDiv.prepend(p);
                 gifDiv.prepend(image);
+                gifDiv.prepend(innerDiv);
                 $("#gifs").prepend(gifDiv);
             }
 });
 
 }
 
+function pauseGif() {
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  };
+
+$(document).on("click", ".download-button", downloadGif);
+
+$(document).on("click", ".myGif", pauseGif);
 
 $(document).on("click", ".gif-search", gifSearch);
+
 
 gifButton();
 
 })
 
+
+
 //create a favorite button for each gif that is loaded 
 //button will prepend to favoite section
-/*
-    var downloading = browser.downloads.download(
-        options                   // object
-        )
-        var downloadUrl = "https://example.org/image.png";
-
-var downloading = browser.downloads.download({
-  url : downloadUrl,
-  filename : 'my-image-again.png',
-  conflictAction : 'uniquify'
-});
-
-    */
+//download with link?
